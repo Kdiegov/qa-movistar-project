09f9eb89751d50f9fcf4be4178f0a275a88b2d5e
Kdiegov <kdiegov1800@alumno.ipn.mx>
2026-02-20 12:41:52 -0600

    segundo commit

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,12 @@
 import { test, expect } from '@playwright/test';
+import { beforeEach } from 'node:test';
+
+test.beforeEach (async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
